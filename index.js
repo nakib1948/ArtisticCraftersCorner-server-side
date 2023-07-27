9aad3e37c57a9ea22f5d42288587998181656bda
nakib1948 <hayathossain.jtp@gmail.com>
2023-07-27 16:28:10 +0600

    classes dataload complete

---
+++ index.js
@@ -0,0 +1,71 @@
+const express = require("express");
+const cors = require("cors");
+require("dotenv").config();
+const app = express();
+const bodyParser = require("body-parser");
+const port = process.env.PORT || 3000;
+const { MongoClient, ServerApiVersion,ObjectId } = require("mongodb");
+const jwt = require("jsonwebtoken");
+app.use(bodyParser.json());
+app.use(cors());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.qxayaa3.mongodb.net/?retryWrites=true&w=majority`;
+
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+
+const verifyJWT = (req, res, next) => {
+  
+  const authorization = req.headers.authorization;
+  if (!authorization) {
+    return res.send({ error: true, message: "unauthorized access" });
+  }
+  const token = authorization.split(" ")[1];
+  jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (error, decoded) => {
+    if (error) {
+      return res
+        .status(403)
+        .send({ error: true, message: "unauthorized access" });
+    }
+    req.decoded = decoded;
+    next();
+  });
+};
+
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+  
+
+    const classCollection = client.db("summercamp").collection("classes");
+
+   
+   app.get('/classes',async(req,res)=>{
+
+     const result = await classCollection.find().sort({ enrolled: -1 }).toArray()
+     const cutdata=  result.slice(0,6)
+   // console.log(result)
+     res.send(cutdata)
+
+   })
+
+
+    await client.db("admin").command({ ping: 1 });
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
+  } finally {
+   
+  }
+}
+run().catch(console.dir);
+
+app.listen(port, function () {
+  console.log(`Listening on port ${port}`);
+});
